ef1970313ac602af172a2cf136bcb9aa31d79186
jwharbeson <164430971+jwharbeson@users.noreply.github.com>
2024-09-22 21:40:07 -0500

    Delete JWH_django_project_updated directory

--- JWH_django_project_updated/mysite/mysite/urls.py
+++
@@ -1,9 +0,0 @@
-from django.contrib import admin
-from django.urls import path
-from myapp import views
-
-urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('time/', views.current_time, name='current_time'),
-    path('', views.home, name='home'),
-]
